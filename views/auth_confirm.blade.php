8a5651215a0f9b5088d9867ccbbc5d24d235f8f0
Davood Jafari <to.jafari@gmail.com>
2023-04-24 17:56:28 +0330

    2023-04-24 17-05

--- views/auth_confirm.blade.php
+++ views/auth_confirm.blade.php
@@ -3,7 +3,7 @@ we received a request to log in on <code>{{ $url }}</code> with your Telegram ac
 To authorize this request, use the <b>'Confirm'</b> button below, Or enter <code>{{ $code }}</code>.
 
 <b>Username</b>: {{ $username }}
-<b>Browser</b>: {{ $ua->ua->family }} {{ $ua->ua->major }} on {{ $ua->os->family }} {{ $ua->os->major }},{{ $ua->os->minor }}</code>
+<b>Browser</b>: {{ $ua->ua->family }} {{ $ua->ua->major }} on {{ $ua->os->family }} {{ $ua->os->major }},{{ $ua->os->minor }}
 <b>IP</b>: {{ $ip }}
 
 If you didn't request this, use the 'Decline' button or ignore this message.
