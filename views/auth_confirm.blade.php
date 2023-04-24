8af1beb2c290eea0777044086070da3e935c42ac
Davood Jafari <to.jafari@gmail.com>
2023-04-24 17:57:43 +0330

    2023-04-24 17-05

--- views/auth_confirm.blade.php
+++ views/auth_confirm.blade.php
@@ -1,9 +1,11 @@
 we received a request to log in on <code>{{ $url }}</code> with your Telegram account.
 
-To authorize this request, use the <b>'Confirm'</b> button below, Or enter <code>{{ $code }}</code>.
+To authorize this request, use the <b>'Confirm'</b> button below
+Or
+Enter <code>{{ $code }}</code>.
 
 <b>Username</b>: {{ $username }}
-<b>Browser</b>: {{ $ua->ua->family }} {{ $ua->ua->major }} on {{ $ua->os->family }} {{ $ua->os->major }},{{ $ua->os->minor }}
+<b>Browser</b>: {{ $ua->ua->family }} {{ $ua->ua->major }} on {{ $ua->os->family }} {{ $ua->os->major }}.{{ $ua->os->minor }}
 <b>IP</b>: {{ $ip }}
 
 If you didn't request this, use the 'Decline' button or ignore this message.
