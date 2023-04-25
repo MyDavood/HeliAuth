d38cc6365eb11ee14997366a002e845eaf3621be
Davood Jafari <to.jafari@gmail.com>
2023-04-25 17:50:03 +0330

    2023-04-25 17-05

--- src/Controllers/ChangeStatusController.php
+++ src/Controllers/ChangeStatusController.php
@@ -29,7 +29,7 @@ class ChangeStatusController
             ttl: now()->addMinutes(config('heliAuth.hash_ttl')),
         );
         Cache::delete($request->hash);
-        event(new AuthorizeStatusEvent(
+        broadcast(new AuthorizeStatusEvent(
             oldHash: $request->hash,
             newHash: $newHash,
         ));
